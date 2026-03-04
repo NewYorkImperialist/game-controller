a988d2221d1fbee8d7d9761262b2aa9d5007a107
Jayden Lin <168321799+NewYorkImperialist@users.noreply.github.com>
2026-03-04 00:16:27 -0500

    calculate centers in driver to account for offset/drift

--- src/driver.py
+++ src/driver.py
@@ -1,18 +1,33 @@
 import serial
 
+SAMPLES = 50
+
 ser = serial.Serial('/dev/tty.usbserial-0001', 115200, timeout=1)
 print("Connected! Waiting for data...")
 
-while True:
+centers = [0, 0, 0, 0]
+for _ in range(SAMPLES):
     packet = ser.read(11)
     if len(packet) == 11:
-        joy1_x = packet[1] | (packet[2] << 8)
-        joy1_y = packet[3] | (packet[4] << 8)
-        joy2_x = packet[5] | (packet[6] << 8)
-        joy2_y = packet[7] | (packet[8] << 8)
-        key = packet[9]
-        checksum = packet[10]
-        print(f"J1: ({joy1_x}, {joy1_y}) J2: ({joy2_x}, {joy2_y}) Key: {key} Checksum: {checksum:02X}")
-    else: 
+        centers[0] += packet[1] | (packet[2] << 8)
+        centers[1] += packet[3] | (packet[4] << 8)
+        centers[2] += packet[5] | (packet[6] << 8)
+        centers[3] += packet[7] | (packet[8] << 8)
+    else:
         print("(incomplete packet)")
+centers = [i / SAMPLES for i in centers]
+print(centers)
+
+#while True:
+#    packet = ser.read(11)
+#    if len(packet) == 11:
+#        joy1_x = packet[1] | (packet[2] << 8)
+#        joy1_y = packet[3] | (packet[4] << 8)
+#        joy2_x = packet[5] | (packet[6] << 8)
+#        joy2_y = packet[7] | (packet[8] << 8)
+#        key = packet[9]
+#        checksum = packet[10]
+#        print(f"J1: ({joy1_x}, {joy1_y}) J2: ({joy2_x}, {joy2_y}) Key: {key} Checksum: {checksum:02X}")
+#    else: 
+#        print("(incomplete packet)")
 
